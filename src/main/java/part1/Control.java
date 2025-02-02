f723c97a716a1a7961bcc08e837e52e5d1204541
LaaSofiaa <laura.gil-c@mail.escuelaing.edu.co>
2025-02-02 18:55:35 -0500

    PARTE 1

--- src/main/java/part1/Control.java
+++ src/main/java/part1/Control.java
@@ -1,7 +1,7 @@
 package part1;
 
 /**
- *
+ * pausa y reanudación de los hilos
  */
 public class Control extends Thread {
 
@@ -12,6 +12,7 @@ public class Control extends Thread {
     private final int NDATA = MAXVALUE / NTHREADS;
 
     private PrimeFinderThread pft[];
+    private final Object lock = new Object(); //objeto para poder sincronizar
 
     private Control() {
         super();
@@ -19,10 +20,10 @@ public class Control extends Thread {
 
         int i;
         for(i = 0;i < NTHREADS - 1; i++) {
-            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA);
+            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA, lock);
             pft[i] = elem;
         }
-        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1);
+        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1, lock);
     }
 
     public static Control newControl() {
@@ -34,6 +35,61 @@ public class Control extends Thread {
         for(int i = 0;i < NTHREADS;i++ ) {
             pft[i].start();
         }
+
+        while (true) {
+            try {
+                Thread.sleep(TMILISECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            synchronized (lock) {
+                System.out.println("Pausando todos los hilos...");
+                // Detener todos los hilos
+                for (PrimeFinderThread thread : pft){
+                    thread.pauseThread();
+                }
+
+                int totalPrimes = 0;
+                // Mostrar primos encontrados
+                for(PrimeFinderThread thread : pft){
+                    totalPrimes += thread.getPrimes().size();
+                }
+                System.out.println("Numero de Primos encontrados por el momento: " + totalPrimes);
+
+                // Verificar si todos los hilos han terminado
+                boolean allThreadsFinished = true;
+                for (PrimeFinderThread thread : pft) {
+                    if (thread.isAlive()) {
+                        allThreadsFinished = false;
+                        break;
+                    }
+                }
+
+                if (allThreadsFinished) {
+                    System.out.println("Todos los hilos han terminado. Finalizando el programa.");
+                    break; // Salir del bucle principal
+                }
+
+                // Esperar que el usuario presione ENTER
+                System.out.println("Presione ENTER para continuar!");
+
+                try{
+                    System.in.read();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+                System.out.println("Reanudando todos los hilos...");
+
+                // Reanudar todos los hilos
+                for (PrimeFinderThread thread : pft) {
+                    thread.resumeThread();
+                }
+
+            }
+        }
+
     }
 
 }
--- src/main/java/part1/PrimeFinderThread.java
+++ src/main/java/part1/PrimeFinderThread.java
@@ -4,23 +4,37 @@ package part1;
 import java.util.LinkedList;
 import java.util.List;
 
+
 public class PrimeFinderThread extends Thread{
 
 
     int a,b;
 
     private List<Integer> primes;
+    private final Object lock;
+    private boolean paused = false;
 
-    public PrimeFinderThread(int a, int b) {
+    public PrimeFinderThread(int a, int b, Object lock) {
         super();
         this.primes = new LinkedList<>();
         this.a = a;
         this.b = b;
+        this.lock = lock;
     }
 
     @Override
     public void run(){
         for (int i= a;i < b;i++){
+            synchronized (lock){
+                while(paused){
+                    try{
+                        System.out.println("Hilo " + Thread.currentThread().getId() + " en espera...");
+                        lock.wait();
+                    }catch (InterruptedException e){
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
             if (isPrime(i)){
                 primes.add(i);
                 System.out.println(i);
@@ -43,6 +57,18 @@ public class PrimeFinderThread extends Thread{
 
     public List<Integer> getPrimes() {
         return primes;
+    }
+
+    public void pauseThread(){
+        synchronized (lock){
+           paused = true;
+        }
+    }
+    public void resumeThread(){
+        synchronized (lock){
+            paused = false;
+            lock.notifyAll(); // notifica que salga de wait a los hilos
+        }
     }
 
 }
